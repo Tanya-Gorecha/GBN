00940d8d3e61611001915ba7ac63a0b2a37ae942
Tanya-Gorecha <121540704+Tanya-Gorecha@users.noreply.github.com>
2022-12-28 16:05:03 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function change(){
+    document.getElementById("body").style.backgroundColor="#80D8FF";
+    document.getElementById("body").style.color="black";
+    document.getElementById("h1").innerHTML="every child counts every moment matters";
+    document.getElementById("p1").style.fontVariant="small-caps";
+    document.getElementById("p1").style.color="black";
+  }
+  function changeimage(){
+    document.getElementById("img1").src="https://i.postimg.cc/63qCkJNm/images.jpg";
+  }
+  function changeimageback(){
+     document.getElementById("img1").src="https://i.postimg.cc/SKNZxPhW/images-1.jpg";
+  }
+  
